9e6b20c4bea0884503084c9065a9366b1109a848
loukas isca <l.partatilas@gmail.com>
2025-02-28 16:06:28 +0200

    css for welcome page & navbar

--- todo-frontend/src/components/Navbar.js
+++ todo-frontend/src/components/Navbar.js
@@ -1,9 +1,25 @@
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import '../styles/Navbar.css'
 
 const Navbar = ({ isLoggedIn, onLogout }) => {
+  const [scrolled, setScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true)
+      } else {
+        setScrolled(false)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
   return (
-    <nav className='navbar'>
+    <nav className={`navbar ${scrolled ? 'scrolled' : ''}`}>
       <Link to='/'>Home</Link>
       <Link to='/about'>About</Link>
       {isLoggedIn ? (
--- todo-frontend/src/components/Welcome.js
+++ todo-frontend/src/components/Welcome.js
@@ -1,14 +1,47 @@
 import { Link } from 'react-router-dom'
 import '../styles/Welcome.css'
+import image1 from '../assets/images/image1.jpeg'
+import image3 from '../assets/images/image3.jpg'
+import image4 from '../assets/images/image4.jpeg'
+import image5 from '../assets/images/image5.jpeg'
+import image6 from '../assets/images/image7.jpeg'
 
 const Welcome = () => {
   return (
     <div className='welcome-container'>
-      <h1>Welcome to My Task App</h1>
-      <p>Organize your tasks easily and efficiently!</p>
-      <Link to='/login'>
-        <button className='start-btn'>Get Started</button>
-      </Link>
+      <div className='left-panel'>
+        <div className='mini-frame'>
+          <img src={image1} alt='Description 1' className='mini-image' />
+          <img src={image3} alt='Description 2' className='mini-image' />
+        </div>
+        <div className='mini-frame'>
+          <img src={image4} alt='Description 3' className='mini-image' />
+          <img src={image5} alt='Description 4' className='mini-image' />
+        </div>
+      </div>
+      <div className='right-panel'>
+        <div className='banner'>
+          <img src={image6} alt='Banner' className='banner-image' />
+        </div>
+        <div className='welcome-message'>
+          <h1>Welcome to My Task App</h1>
+          <p>
+            Organize your tasks efficiently and stay productive. Get started by{' '}
+            <Link to='/login' className='login-button'>
+              Logging in
+            </Link>{' '}
+            or learn more on our{' '}
+            <Link to='/about' className='hyperlink'>
+              About
+            </Link>{' '}
+            and{' '}
+            <Link to='/contact' className='hyperlink'>
+              Contact
+            </Link>{' '}
+            pages.
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
